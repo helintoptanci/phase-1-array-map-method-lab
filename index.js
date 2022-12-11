9c6ac261cb7f05b9f8ef563abac81c8a9d2fbdc3
Helin <helintoptanci3@outlook.com>
2022-12-11 18:48:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -11,6 +11,25 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => {
+   
+    let lowerCased = tutorial.toLowerCase();
+
+   
+    let words = lowerCased.split(' ');
+
+    
+    let titleCasedWords = words.map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+
+    
+    return titleCasedWords.join(' ');
+  });
 }
+const titleCasedTutorials = titleCased();
+
+console.log(titleCasedTutorials);
